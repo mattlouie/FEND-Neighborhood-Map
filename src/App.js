48cb434d417bf65516a1a35eee4952f65bbf0eb2
Matthew Louie <mlouie915@gmail.com>
2018-10-23 20:39:03 -0700

    Added promise and markers for venues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'; 
+import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
-    this.renderMap()
+    this.getVenues()
   }
 
   renderMap = () => {
@@ -13,11 +17,39 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "ZHAYJAWWRZRJ2CX3WIAAX3SS4BBSHO5CYPKAWXHG3XDHKBOI",
+      client_secret: "ALY3T2OKQT2MMJYLVXRM1WRWAHAL02BRKU4W5A5OPNVMC5SA",
+      query: "coffee",
+      near: "San Francisco, CA",
+      v: "20180323"
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, this.renderMap())
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
+
   initMap = () => {
     // eslint-disable-next-line
     var map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.773972, lng: -122.431297},
       zoom: 13
+    })
+
+    this.state.venues.map(myVenue => {
+      var marker = new window.google.maps.Marker ({
+        position: {lat: myVenue.venue.location.lat , lng: myVenue.venue.location.lng},
+        map: map
+      })
     })
   }
 
